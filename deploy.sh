4b2c39bbb548adbd09dbd6bd7fe3223039086155
louiscklaw <louiscklaw@gmail.com>
2019-10-13 01:30:47 +0800

    update to fix source code dir,

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-gcloud functions deploy google-trends-crawler --source --source ./src  --entry-point main_routes --runtime nodejs8 --trigger-http --project louislabs-com
+gcloud functions deploy google-trends-crawler --source ./src  --entry-point main_routes --runtime nodejs8 --trigger-http --project louislabs-com
